45da8722b821ce92989f2e3cc528285afa2bc589
Martins Obakoma <obakoma@martins>
2025-02-11 00:13:33 +0000

    Task 2 - Formatting the output modified

--- lab1/logger-lab/src/Task2/task2.py
+++ lab1/logger-lab/src/Task2/task2.py
@@ -1,4 +1,5 @@
 import logging
 
-logging.basicConfig(format='%(process)d-%(levelname)s-%(message)s')
-logging.warning('This is a Warning')
+logging.basicConfig(format='%(asctime)s - %(message)s',
+level=logging.INFO)
+logging.info('Admin logged in')
