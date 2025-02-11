3c5be134ccd518b629842c38675b17f2db492976
Martins Obakoma <obakoma@martins>
2025-02-11 00:22:10 +0000

    Task3's task3.py file modified

--- lab1/logger-lab/src/Task3/task3.py
+++ lab1/logger-lab/src/Task3/task3.py
@@ -1,3 +1,7 @@
 import logging
-name = 'John'
-logging.error(f'{name} raised an error')
+a = 5
+b = 0
+try:
+    c = a / b
+except Exception as e:
+    logging.error("Exception occurred", exc_info=True)
