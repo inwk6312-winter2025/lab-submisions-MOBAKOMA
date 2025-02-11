ef3d348e14cda09ca325ae4c09eca85d4d862304
Martins Obakoma <obakoma@martins>
2025-02-11 00:27:32 +0000

    task3.py modified2

--- lab1/logger-lab/src/Task3/task3.py
+++ lab1/logger-lab/src/Task3/task3.py
@@ -1,8 +1,4 @@
 import logging
 
-a = 5
-b = 0
-try:
-    c = a / b
-except Exception as e:
-    logging.exception("Exception occurred")
+logger = logging.getLogger('example_logger')
+logger.warning('This is a warning')
